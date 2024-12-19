e2d88161ccad0b4f4c917e16b27f11c866c9d8f8
goodsir6 <eduardo.bomsenhor.info@gmail.com>
2024-12-19 20:52:36 -0300

    feat: bearer #Eduardo

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
-    Authorization: `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODBlNmVmZDFmNjM2YjQ1N2Q5ZWZmOTFjMWQyYmY2NCIsIm5iZiI6MTczNDM2Njg2MS41NjQsInN1YiI6IjY3NjA1NjhkYWNhMzU0OWU1NGY2NzdkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DxdjnF-rXRQBFWfpt6p35Oi-jmZwn5H4iUpa46q5BDQ`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODBlNmVmZDFmNjM2YjQ1N2Q5ZWZmOTFjMWQyYmY2NCIsIm5iZiI6MTczNDM2Njg2MS41NjQsInN1YiI6IjY3NjA1NjhkYWNhMzU0OWU1NGY2NzdkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DxdjnF-rXRQBFWfpt6p35Oi-jmZwn5H4iUpa46q5BDQ`,
   },
 });
 
